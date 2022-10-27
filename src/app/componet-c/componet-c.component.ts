a5a27812601fe41368c3328603c5f3fbf0062760
Isham Mohamed <34007900+isham-mohamed@users.noreply.github.com>
2022-10-27 16:03:42 +0530

    added comp C (#3)

---
+++ src/app/componet-c/componet-c.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componet-c',
+  templateUrl: './componet-c.component.html',
+  styleUrls: ['./componet-c.component.scss']
+})
+export class ComponetCComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
